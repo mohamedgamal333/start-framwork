a843b0bb6fd860418770c9af29c0055866484773
mohamedgamal333 <mohamedgamal5107@gmail.com>
2025-02-24 10:21:41 +0200

    make change in navbar using @hostlistener method

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
@@ -8,5 +8,18 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  scroll:boolean=false; // make bing in html file
+  // make a function to check the scroll using HostListener method on the window
+  @HostListener('window:scroll')onScroll()
+    {
+      if(scrollY>0){
+        this.scroll=true;
+      }
+      else{
+this.scroll=false;
+      }
+console.log('scrolling');
+
+    }
 
 }
